820de1ccef2a418569db0dd01f4ecfed51d80692
cyruu <cyruz.mhr09@gmail.com>
2024-07-19 11:24:49 +0545

    tt

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import CloseIcon from "@mui/icons-material/Close";
+import { useNavigate } from "react-router";
 const Search = ({ setSearchStatus }) => {
+  const navigate = useNavigate();
+
+  const [searchInput, setSearchInput] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate(`/${searchInput}`);
+  }
   return (
     <form
-      action="/search"
-      method="get"
+      onSubmit={handleSubmit}
       className="border-[1px] h-max my-auto border-gray-300 rounded-full px-2 py-2 flex  items-center "
     >
       <SearchIcon className="mr-1" />
       <input
-        name="searchinput"
+        // name="searchinput"
         type="text"
         placeholder="Search"
         className="outline-none flex-initial w-[250px] mb-1 md:w-[15rem] xl:w-[25rem] sm:w-[200px] md:m-0"
         style={{ fontSize: ".9rem" }}
+        value={searchInput}
+        onChange={({ target }) => setSearchInput(target.value)}
         required
       />
     </form>
